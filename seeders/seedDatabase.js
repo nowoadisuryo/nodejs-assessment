0f76c727acc0b05d15e26aedb161dd29b4119f8f
Nowo Adi Suryo <nowoadisuryo@gmail.com>
2025-03-18 16:02:01 +0700

    Update the seeder

--- seeders/seedDatabase.js
+++ seeders/seedDatabase.js
@@ -11,17 +11,28 @@ const seedDatabase = async () => {
 
     // Seed Teachers
     const teacher1 = await Teacher.create({ email: 'teacherken@gmail.com' });
-    const teacher2 = await Teacher.create({ email: 'teacherjane@gmail.com' });
+    const teacher2 = await Teacher.create({ email: 'teacherjoe@gmail.com' });
 
     // Seed Students
-    const student1 = await Student.create({ email: 'student1@example.com' });
-    const student2 = await Student.create({ email: 'student2@example.com' });
-    const student3 = await Student.create({ email: 'student3@example.com' });
-    const student4 = await Student.create({ email: 'student4@example.com' });
+    await Student.create({ email: 'studentjon@gmail.com' });
+    await Student.create({ email: 'studenthon@gmail.com' });
+    const student3 = await Student.create({
+      email: 'commonstudent1@gmail.com',
+    });
+    const student4 = await Student.create({
+      email: 'commonstudent2@gmail.com',
+    });
+    const student5 = await Student.create({
+      email: 'student_only_under_teacher_ken@gmail.com',
+    });
+    await Student.create({ email: 'studentagnes@gmail.com' });
+    await Student.create({ email: 'studentmiche@gmail.com' });
+    const student8 = await Student.create({ email: 'studentbob@gmail.com' });
+    await Student.create({ email: 'studentmary@gmail.com' });
 
     // Associate Students with Teachers (Many-to-Many Relationship)
-    await teacher1.addStudents([student1, student2, student4]);
-    await teacher2.addStudents([student2, student3]);
+    await teacher1.addStudents([student3, student4, student5, student8]);
+    await teacher2.addStudents([student3, student4]);
 
     logger.info('✅ Database seeding completed successfully!');
     process.exit(0); // Exit script after seeding
