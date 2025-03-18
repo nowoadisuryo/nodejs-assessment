f886e47106c5f37bd3d1e8c4e110fa6e6ea360d6
Nowo Adi Suryo <nowoadisuryo@gmail.com>
2025-03-18 12:53:20 +0700

    Add unit test - student repository

---
+++ test/repositories/StudentRepository.test.js
@@ -0,0 +1,92 @@
+const StudentRepository = require('../../src/repositories/StudentRepository');
+const { MissingValueError } = require('../../src/utils/assert');
+
+describe('StudentRepository', () => {
+  let opts;
+  let clock;
+  let studentRepository;
+  const sandbox = sinon.createSandbox();
+  const teachers = [
+    { id: 1, email: 'teacher1@gmail.com' },
+    { id: 2, email: 'teacher2@gmail.com' },
+  ];
+  const students = [
+    { id: 1, email: 'student1@gmail.com', suspended: false },
+    { id: 2, email: 'student2@gmail.com', suspended: false },
+  ];
+  const inputTeacher = 'teacher1@gmail.com';
+  const inputStudents = ['student1@gmail.com', 'student3@gmail.com'];
+  const inputStudent = 'student1@gmail.com';
+
+  beforeEach(() => {
+    clock = sinon.useFakeTimers(new Date('2025-03-16'));
+
+    opts = {
+      models: {
+        teacherModel: {
+          findOne: sandbox.stub(),
+          addStudents: sandbox.stub(),
+          findAll: sandbox.stub(),
+        },
+        studentModel: {
+          findOne: sandbox.stub(),
+          findAll: sandbox.stub(),
+        },
+      },
+      logger: {
+        info: sandbox.stub(),
+        error: sandbox.stub(),
+      },
+    };
+
+    studentRepository = new StudentRepository(opts);
+  });
+
+  afterEach(() => {
+    clock.restore();
+    sandbox.restore();
+  });
+
+  describe('#suspendStudent', () => {
+    it('should throw error when student not found', async () => {
+      opts.models.studentModel.findOne.resolves(null);
+
+      const actual = studentRepository.suspendStudent(inputStudent);
+
+      await expect(actual).to.eventually.rejectedWith(Error);
+    });
+
+    it('should throw error when student was already suspended', async () => {
+      opts.models.studentModel.findOne.resolves({ suspended: true });
+
+      const actual = studentRepository.suspendStudent(inputStudent);
+
+      await expect(actual).to.eventually.rejectedWith(Error);
+    });
+
+    it('should success suspend a student', async () => {
+      opts.models.studentModel.findOne.resolves({
+        suspended: false,
+        update: sinon.stub(),
+        save: sinon.stub(),
+      });
+
+      const actual = await studentRepository.suspendStudent(inputStudent);
+
+      expect(actual.update).to.be.calledOnce;
+      expect(actual.save).to.be.calledOnce;
+    });
+  });
+
+  describe('#findByEmailsNotSuspended', () => {
+    it('should find unsuspend students', async () => {
+      opts.models.studentModel.findAll.resolves(students);
+
+      const actual =
+        await studentRepository.findByEmailsNotSuspended(inputStudents);
+
+      expect(actual).to.be.an('array');
+      expect(actual).to.have.lengthOf(2);
+    });
+  });
+});
